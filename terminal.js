bd0422543af5f8f84b40f6416904746b4b9bd0f9
Akihiro <36966603+GlowingObsidian@users.noreply.github.com>
2022-10-06 10:08:05 +0530

    Minor changes

--- terminal.js
+++ terminal.js
@@ -1,9 +1,16 @@
 function terminal(id) {
-  this.init = function () {
-    document.getElementById(id).innerHTML =
-      '<p style="text-align: center;">-: VLL Console v2.0 :-</p>';
+  this.log = function (data) {
+    current = document.getElementById(id).innerHTML;
+    document.getElementById(id).innerHTML = current.replace(
+      '<br>$&nbsp;<input type="text" id="command">',
+      ""
+    );
+
+    document.getElementById(id).innerHTML += "<br>" + data;
     document.getElementById(id).innerHTML +=
-      '<br>$<input type="text" id="command">';
+      '<br>$&nbsp;<input type="text" id="command">';
+    document.getElementById(id).scrollTop =
+      document.getElementById(id).scrollHeight;
 
     var enter = document.getElementById("command");
     enter.addEventListener("keypress", function (event) {
@@ -15,17 +22,11 @@ function terminal(id) {
     document.getElementById("command").value = "";
   };
 
-  this.log = function (data) {
-    current = document.getElementById(id).innerHTML;
-    document.getElementById(id).innerHTML = current.replace(
-      '<br>$<input type="text" id="command">',
-      ""
-    );
-    document.getElementById(id).innerHTML += "<br>" + data;
+  this.init = function () {
+    document.getElementById(id).innerHTML =
+      '<p style="text-align: center;">-: VLL Console v2.0 :-</p>';
     document.getElementById(id).innerHTML +=
-      '<br>$<input type="text" id="command">';
-    document.getElementById(id).scrollTop =
-      document.getElementById(id).scrollHeight;
+      '<br>$&nbsp;<input type="text" id="command">';
 
     var enter = document.getElementById("command");
     enter.addEventListener("keypress", function (event) {
